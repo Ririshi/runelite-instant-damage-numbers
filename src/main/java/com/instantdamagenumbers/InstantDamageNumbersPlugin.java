3f9917fa2c17504f476032511d3280d36c911c39
Andele Swierstra <andele.rien@gmail.com>
2021-09-27 18:39:17 +0200

    Initial plugin setup

--- src/main/java/com/instantdamagenumbers/InstantDamageNumbersPlugin.java
+++ src/main/java/com/instantdamagenumbers/InstantDamageNumbersPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.instantdamagenumbers;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Instant Damage Numbers"
 )
-public class ExamplePlugin extends Plugin
+public class InstantDamageNumbersPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private InstantDamageNumbersConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	InstantDamageNumbersConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(InstantDamageNumbersConfig.class);
 	}
 }
